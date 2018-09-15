fefb882384c478e6fe3970fa7e10370f711a4630
Yuan Ji <yj.the.coder@gmail.com>
2018-09-15 16:56:30 -0400

    Set up branches

---
+++ assignment_1/assignment_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment_1
+//
+//  Created by YUAN JI on 2018-09-15.
+//  Copyright © 2018 YUAN JI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
