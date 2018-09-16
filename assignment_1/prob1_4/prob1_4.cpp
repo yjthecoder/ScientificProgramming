7facb33aba4cb758d51d5113fc564ab78f2a6345
Yuan Ji <yj.the.coder@gmail.com>
2018-09-16 19:48:51 -0400

    Problem 1.4 all done

---
+++ assignment_1/prob1_4/prob1_4.cpp
@@ -0,0 +1,16 @@
+/*****************************************************************/
+/* Problem 1.4                                                   */
+/* The program declares four 2 × 2 matrices of double precision  */
+/* floating point numbers,A,B,C,D, and assigns values to the     */
+/* entries of A and B. Let C = A + B, and D=AB. The program then */
+/* calculate the entries of C and D, and then prints the entries */
+/* of these matrices to screen.                                  */
+/*****************************************************************/
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
