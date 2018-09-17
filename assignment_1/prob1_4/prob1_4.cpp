62dd006ea1082e888b0edac26eacfa79a6bb5623
Yuan Ji <yj.the.coder@gmail.com>
2018-09-16 20:28:40 -0400

    Problem 1.4 all done

--- assignment_1/prob1_4/prob1_4.cpp
+++ assignment_1/prob1_4/prob1_4.cpp
@@ -9,8 +9,39 @@
 
 #include <iostream>
 
+// using the std namespace to save all the typing of std
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Declare four 2 × 2 matrices as required
+    double A[2][2] = {{1.1 ,1.1}, {1.1 ,1.1}};
+    double B[2][2] = {{2.2 ,2.2}, {2.2, 2.2}};
+    double C[2][2];
+    double D[2][2];
+    
+    
+    // Calculate entries of C = A + B
+    C[0][0] = A[0][0] + B[0][0];
+    C[0][1] = A[0][1] + B[0][1];
+    C[1][0] = A[1][0] + B[1][0];
+    C[1][1] = A[1][1] + B[1][1];
+    
+    
+    // Calculate entries of D = AB
+    D[0][0] = A[0][0] * B[0][0] + A[0][1] * B[1][0];
+    D[0][1] = A[0][0] * B[0][1] + A[0][1] * B[1][1];
+    D[1][0] = A[1][0] * B[0][0] + A[1][1] * B[1][0];
+    D[1][1] = A[1][0] * B[0][1] + A[1][1] * B[1][1];
+    
+    // Print C to the screen
+    cout << "The entries of matrix C : "<< endl;
+    cout << "[" << C[0][0] << "] " << "[" << C[0][1] << "]" << endl;
+    cout << "[" << C[1][0] << "] " << "[" << C[1][1] << "]" << endl;
+    
+    // Print D to the screen
+    cout << "The entries of matrix D : "<< endl;
+    cout << "[" << D[0][0] << "] " << "[" << D[0][1] << "]" << endl;
+    cout << "[" << D[1][0] << "] " << "[" << D[1][1] << "]" << endl;
+    
+    
     return 0;
 }
