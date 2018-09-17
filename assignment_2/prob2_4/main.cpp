e393cd1de4df4c7633318d833d15b4b2ca36d59a
Yuan Ji <yj.the.coder@gmail.com>
2018-09-17 12:54:29 -0400

    Assignment 2 file structure set upfinished

---
+++ assignment_2/prob2_4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prob2_4
+//
+//  Created by YUAN JI on 2018-09-17.
+//  Copyright © 2018 YUAN JI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
