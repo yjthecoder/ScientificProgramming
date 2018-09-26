d15260191623d9c97fb4b07b76efe4ebbe77a8e4
Yuan Ji <yj.the.coder@gmail.com>
2018-09-26 11:32:29 -0400

    Problem 2.4 all finished

--- assignment_2/prob2_4/prob2_4.cpp
+++ assignment_2/prob2_4/prob2_4.cpp
@@ -1,15 +1,23 @@
-//
-//  main.cpp
-//  prob2_4
-//
-//  Created by YUAN JI on 2018-09-17.
-//  Copyright © 2018 YUAN JI. All rights reserved.
-//
+/***********************************************************/
+/* Problem 2.4                                             */
+/* The program defines two vectors u, v and two matrices   */
+/* A and B, use them to calculate other 3 vectors:         */
+/* x = u − v                                               */
+/* y = Au                                                  */
+/* z = Au -v                                               */
+/* and two other matrices:                                 */
+/* C = 4A - 3B                                             */
+/* D = AB                                                  */
+/* and prints the result to screen, with each entry taking */
+/* five characters' spaces and right justfied              */
+/***********************************************************/
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     
+    // Initialize the given data
     double u[3] = {1.0, 2.0, 3.0};
     double v[3] = {6.0, 5.0, 4.0};
     double A[3][3] = {{1.0, 5.0, 0.0},
@@ -40,23 +48,118 @@ int main(int argc, const char * argv[]) {
         x[i] = u[i] - v[i];
     }
     
+    
+    // Print x to the screen
+    cout << "Vector x=u-v :"<< endl;
+    
+    for (int i = 0; i < 3; i++) {
+        if (i / 2 == 0) {
+            
+            // set the fixed length of output
+            cout.width(5);
+            
+            // seperate each entry with space
+            // if the entry is not at end of line
+            cout << x[i] << " ";
+        } else {
+            
+            // start a new line after each row
+            cout.width(5);
+            cout << x[i] << "\n";
+        }
+        
+    }
+    cout << endl;
+    
     // Calculate vector y = Au
     for (int i=0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            y[i][j] += A[i][j] * u[j];
+            y[i] += A[i][j] * u[j];
         }
     }
     
+    // Print y to the screen
+    cout << "Vector y=Au :"<< endl;
+    for (int i = 0; i < 3; i++) {
+        if (i != 2) {
+            cout.width(5);
+            cout << y[i] << " ";
+        } else {
+            cout.width(5);
+            cout << y[i] << "\n";
+        }
+    }
+    
+    // print a blank line to seperate each result
+    cout << endl;
     
     
+    // Calculate vector z = Au - v
+    for (int i=0; i < 3; i++) {
+        z[i] = y[i] - v[i];
+    }
+    
+    // Print z to the screen
+    cout << "Vector z=Au-v :"<< endl;
+    for (int i = 0; i < 3; i++) {
+        if (i != 2) {
+            cout.width(5);
+            cout << z[i] << " ";
+        } else {
+            cout.width(5);
+            cout << z[i] << "\n";
+        }
+    }
+    cout << endl;
+
+    
+    // Calculate matrix C = 4A - 3B
+    for (int i=0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            C[i][j] = 4 * A[i][j]  - 3 * B[i][j];
+        }
+    }
+    
+    // Print C to the screen
+    cout << "Matrix C=4A - 3B: "<< endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (j / 2 == 0) {
+                cout.width(5);
+                cout << C[i][j] << " ";
+            } else {
+                cout.width(5);
+                cout << C[i][j] << "\n";
+            }
+        }
+    }
+    cout << endl;
+
     
     
+    // Calculate matrix D = AB
+    for (int i=0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                D[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
     
-    
-    
-    
-    
-    
+    // Print D to the screen
+    cout << "Matrix D=AB: "<< endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (j / 2 == 0) {
+                cout.width(5);
+                cout << D[i][j] << " ";
+            } else {
+                cout.width(5);
+                cout << D[i][j] << "\n";
+            }
+        }
+    }
+    cout << endl;
     
     return 0;
 }
