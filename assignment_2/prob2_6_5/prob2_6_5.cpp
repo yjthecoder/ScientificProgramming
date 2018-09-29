f234bd130f5b0140dd1eccd8990ead717041888d
Yuan Ji <yj.the.coder@gmail.com>
2018-09-29 12:05:32 -0400

    2.6.5 all done

--- assignment_2/prob2_6_5/prob2_6_5.cpp
+++ assignment_2/prob2_6_5/prob2_6_5.cpp
@@ -4,7 +4,7 @@
 /* difference. The iteration terminates when            */
 /* |x_next-x_prev| < ε.                                 */
 /* We then recompile and run the program with different */
-/* value of ε to verify the result.                     */
+/* values of ε to verify the result.                    */
 /********************************************************/
 
 #include <iostream>
@@ -20,54 +20,58 @@ int main(int argc, const char * argv[]) {
     double x_prev;
     double x_next;
     
-    // Initialize the threshold epsilon
-    double epsi = 1.0e10 * DBL_EPSILON;
-    
-    // initialize the x0 with given initial guess 0
+    // Initialize the x0 with given initial guess 0
     x_prev = 0;
     x_next = x_prev - (exp(x_prev) + pow(x_prev, 3) - 5)
-    / (exp(x_prev) + 2 * pow(x_prev, 2));
+                       / (exp(x_prev) + 2 * pow(x_prev, 2));
     
+    
+    // Print out the first two guess no matter what
+    cout.width(8);
     cout << fixed << x_prev << " " << x_next << " ";
     
-    // Declare a variable for the difference of
-    // the two xi before the current guess
-    //    double prevXdiff;
+    // Initialize the first difference x1-x0
+    double prevXdiff = fabs(x_next);
     
+    // Initialize the threshold epsilon
+    double epsi = 1.0e5 * DBL_EPSILON;
+    
+    // Keep track of the number of guesses after 0
     int counter = 1;
     
-    while ((x_next - x_prev) >= epsi) {
+    while (!(fabs(x_next - x_prev) < epsi)) {
+        
+        // Advance to the next guess
+        x_prev = x_next;
         
         x_next = x_prev - (exp(x_prev) + pow(x_prev, 3) - 5)
-        / (exp(x_prev) + 2 * pow(x_prev, 2));
+                           / (exp(x_prev) + 2 * pow(x_prev, 2));
         
-        //        if (i == 1) {
-        //            prevXdiff = fabs(x_next);
-        //        }
+        // Sych counter with current guess
+        counter++;
         
-        //        // Check if the difference between two
-        //        // adjacent xi keeps decrease as i
-        //        // increases, i.e. guess converges to root
-        //        if (i > 1) {
-        //
-        //            // Initialize the difference of current guess
-        //            double curXdiff = fabs(x_next - x_prev);
-        //
-        //            // Get the differece of difference
-        //            double diffOfDiffX = curXdiff - prevXdiff;
-        //
-        //            // Check if it is shrinking
-        //            assert(diffOfDiffX < 100 * DBL_EPSILON);
-        //
-        //            // Update prevXdiff
-        //            prevXdiff = curXdiff;
-        //        }
         
-        if (i == 1) {
+        // The modified confidence check from 2.6.3
+        // here it also prevent infinite loop if
+        // the guesses do not converge
+       
+        // Calculate the difference between current
+        // guess and the guess before
+        double curXdiff = fabs(x_next - x_prev);
             
-            cout.width(8);
-            cout << fixed << x_prev << " " << x_next << " ";
-        } else if (i % 10 == 9) {
+        // Get the differece of difference
+        double diffOfDiffX = curXdiff - prevXdiff;
+        
+        // Check if it is shrinking
+        assert(diffOfDiffX < 100 * DBL_EPSILON);
+            
+        // Update prevXdiff
+        prevXdiff = curXdiff;
+        
+        
+        
+        // Print out the guess
+        if (counter % 10 == 9) {
             
             // Make sure each line show ten values
             cout.width(8);
@@ -77,10 +81,11 @@ int main(int argc, const char * argv[]) {
             cout << x_next << " ";
         }
         
-        // Advance to the next guess
-        x_prev = x_next;
-        
     }
+    
+    // Print a blank line to make output easy
+    // to be visually separated from each other
+    cout << endl;
     
     return 0;
 }
