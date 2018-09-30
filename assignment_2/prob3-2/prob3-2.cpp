10cc807d15093231d63b45601e4daee8977cda0a
Yuan Ji <yj.the.coder@gmail.com>
2018-09-30 11:50:13 -0400

    3.2 all donw

--- assignment_2/prob3-2/prob3-2.cpp
+++ assignment_2/prob3-2/prob3-2.cpp
@@ -8,28 +8,34 @@
 
 #include <iostream>
 #include <fstream>
-using namespace std;
+
 int main(int argc, char* argv[]) {
     
-    ifstream read_file("x_and_y.dat");
+    std::ifstream read_file("x_and_y.dat");
     
-    if (!read_file.is_open() {
+    if (!read_file.is_open()) {
         
         return 1;
     }
         
-        int number_of_rows = 0;
+    int number_of_rows = 0;
         
-        while(!read_file.eof()) {
-            
-            double dummy1, dummy2, dummy3, dummy4;
-            read_file >> dummy1 >> dummy2;
-            read_file >> dummy3 >> dummy4;
-            number_of_rows++;
+    while(!read_file.eof()) {
+
+        double dummy1, dummy2, dummy3, dummy4;
+        read_file >> dummy1 >> dummy2;
+        read_file >> dummy3 >> dummy4;
+
+        // make sure
+        if (read_file.fail()) {
+            std::cout << "woops, read fails" << std::endl;
+            break;
         }
+        number_of_rows++;
+    }
         
-        std::cout << "Number of rows = "
-        << number_of_rows << "\n";
-        read_file.close();
-        return 0; }
+    std::cout << "Number of rows = "
+              << number_of_rows << "\n";
+    
+    return 0;
 }
