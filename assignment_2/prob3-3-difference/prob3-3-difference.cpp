74c0f786856cb96c1344fdc74b26a2df97a5c46f
Yuan Ji <yj.the.coder@gmail.com>
2018-09-30 20:21:21 -0400

    3-3 all done

--- assignment_2/prob3-3-difference/prob3-3-difference.cpp
+++ assignment_2/prob3-3-difference/prob3-3-difference.cpp
@@ -1,14 +1,16 @@
-/**********************************************************/
-/* Problem 3.3                                            */
-/* This program follows the logical instruction provided  */
-/* in the hand-written description on previous page.      */
-/* Crutial steps are also emphasised by inline comments.  */
-/**********************************************************/
+/**************************************************************/
+/* 3-3-difference                                             */
+/* This program genrates the data of x and its corresponding  */
+/* difference of exp(-x) and y from implicit Euler method     */
+/* Plot generated from the data should give a direct intuition*/
+/* of how close we get to exact function.                     */
+/**************************************************************/
 
 #include <iostream>
 #include <fstream>
 #include <cassert>
 #include <cstdlib>
+#include <cmath>
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -20,14 +22,14 @@ int main(int argc, char* argv[]) {
     // Calculate the step size h with in [0,1]
     double h = (1.0 - 0.0) / N;
     
-    ofstream outputFile("xy.dat");
+    ofstream outputFile("x-difference.dat");
     assert(outputFile.is_open());
     
     // Set precision
     outputFile.precision(6);
     
     // Set scientific notation
-    outputFile.setf(ios::scientific);
+    // outputFile.setf(ios::scientific);
     
     // Set up initial value x0 and y0
     double xi = 0.0;
@@ -45,9 +47,9 @@ int main(int argc, char* argv[]) {
             yi = yi / (1 + h);
         }
         
-        // Print xi and yi on each line followed
-        // by "\n" at end of each line
-        outputFile << xi << " " << yi << "\n";
+        // Print xi and the difference between yi and exp(-xi)
+        // on each line followed by "\n" at end of each line
+        outputFile << xi << " " << fabs(yi - exp(-xi))  << "\n";
         outputFile.flush();
     }
     
